29811c6cd0738c877e211a0ca8e7c992a4d9d0f2
NgLamKha <112705823+NgLamKha@users.noreply.github.com>
2024-04-04 00:08:12 +0700

    building forgot password

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,8 @@
 
 const controller = {};
 const passport = require('./passport');
+const models = require('../models');
+const { Model } = require('sequelize');
 
 controller.show = (req, res) => {
     if(req.isAuthenticated()) {
@@ -44,7 +46,7 @@ controller.isLoggedIn = (req, res, next) => {
         return next();
     }
     res.redirect(`/users/login?reqUrl=${req.originalUrl}`);
-}
+};
 
 controller.register = (req, res, next) => {
     let reqUrl = req.body.reqUrl ? req.body.reqUrl : '/users/my-account';
@@ -58,6 +60,56 @@ controller.register = (req, res, next) => {
             res.redirect(reqUrl);
         })
     })(req, res, next);
-}
+};
+
+controller.showForgotPassword = (req, res) => {
+    res.render('forgot-password');
+};
+
+controller.forgotPassword = async (req, res) => {
+    let email = req.body.email;
+    let user = await models.User.findOne({where: {email}});
+    if(user) {
+        // Tao link
+        const {sign} = require('./jwt');
+        const host = req.header('host');
+        const resetLink = `${req.protocol}://${host}/users/reset?token=${sign(email)}&email=${email}`;
+
+        // Gui mail
+        const {sendForgotPasswordMail} = require('./mail');
+        sendForgotPasswordMail(user, host, resetLink)
+            .then((result) => {
+                console.log('email has been sent');
+                return res.render('forgot-password', {done: true});
+            })
+            .catch(error => {
+                console.log(error.statusCode);
+                return res.render('forgot-password', {message: 'An error has occured when sending to your email. Please check your email address!'});
+            });
+    } else {
+        return res.render('forgot-password', {message: 'Email does not exist!'});
+    }
+};
+
+controller.showResetPassword = (req, res) => {
+    let email = req.query.email;
+    let token = req.query.token;
+    let {verify} = require('./jwt');
+    if(!token || !verify(token)) {
+        return res.render('reset-password', {expired: true});
+    } else {
+        return res.render('reset-password', {email, token});
+    }
+};
+
+controller.resetPassword = async (req, res) => {
+    let email = req.body.email;
+    let token = req.body.token;
+    let bcypt = require('bcrypt');
+    let password = bcypt.hashSync(req.body.password, bcypt.genSaltSync(8));
+
+    await models.User.update({passport}, {where: {email}});
+    res.render('reset-password', {done: true});
+};
 
 module.exports = controller;
